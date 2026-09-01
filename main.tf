22a1d1198530fa22ae8d39f489ade70eb7376889
adityavrm148-Git <adityavrm148@gmail.com>
2026-09-01 16:26:41 +0530

    new code

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "selfrg1" {
+  name = "s-rg1"
+  location = "east us"
+}
+
+resource "azurerm_resource_group" "selfrg2" {
+  name = "s-rg2"
+  location = "east us"
+}
